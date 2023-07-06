a565ce983825f3960f644066461b2fe78392b933
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2023-07-06 16:53:12 +0200

    desktop interface: Fix OpenURI in Core Desktop
    org.freedesktop.portal.OpenURI doesn’t work in core desktop.
    For example, opening Gnome Control Center->Privaty->Location
    Services and clicking on “Privacy Policy” doesn’t work.
    
    This patch fixes it.

--- interfaces/builtin/desktop.go
+++ interfaces/builtin/desktop.go
@@ -335,6 +335,11 @@ dbus (receive, send)
     path=/org/freedesktop/portal/desktop{,/**}
     interface=org.freedesktop.DBus.Properties
     peer=(label=unconfined),
+dbus (send)
+    bus=session
+    interface=org.freedesktop.portal.*
+    path=/org/freedesktop/portal/desktop{,/**}
+    peer=(label=unconfined),
 `
 
 type desktopInterface struct {
